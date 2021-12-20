40cac6a9bdbd929eb8eaa4e6bbdd03b1f11bd460
DungCR7 <94737771+DungCR7@users.noreply.github.com>
2021-12-21 00:24:27 +0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,50 @@
+package StudentManagement;
+
+public class Student {
+    // khai báo
+    private String name;
+    private float gpa;
+
+    //khởi tạo constructor
+    public Student() {
+    }
+
+    public Student(String name, float gpa) {
+        this.gpa = gpa;
+        this.name = name;
+    }
+
+    //getter ,setter
+    public float getGpa() {
+
+        return gpa;
+    }
+    public void setGpa(float gpa) {
+
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    //kiểm tra điểm đỗ trượt ,>=1.5 thì đỗ , ngược lại trượt
+    public boolean checkGPA() {
+        if (gpa >= 1.5) {
+            return true;
+        } else
+            return false;
+    }
+
+    //in ra name,gpa
+    @Override
+    public String toString() {
+        return "Student{" + "name='" + name + '\'' + ", gpa=" + gpa + '}';
+    }
+}
---
+++ StudentManagement.java
@@ -0,0 +1,69 @@
+package StudentManagement;
+
+import java.util.Scanner;
+
+public class StudentManagement {
+    //khai báo
+    private int n;
+    private Student[] students;
+
+    public  StudentManagement(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhap vao so sinh vien: ");
+        int n = input.nextInt();
+        input.nextLine(); // tên sinh viên
+        students = new Student[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Nhập thông tin sinh viên thứ "+(i+1));
+            System.out.print("Name: ");
+            //i < n
+            String name = input.nextLine();
+            System.out.print("Điểm GPA = ");
+            float gpa = input.nextFloat();
+            input.nextLine();
+            // truyền name,gpa
+            students[i] = new Student();
+            students[i].setName(name);
+            students[i].setGpa(gpa);
+        }
+    }
+    //kiemTra
+    public int kiemTraDo(){
+        int count = 0;
+        for(Student st : students){
+            boolean check = st.checkGPA();
+            if(check){
+                count++;
+            }
+        }
+        return count;
+    }
+    public int kiemTraTruot(){
+        int count =0;
+        for(Student st : students){
+            boolean check = st.checkGPA();
+            if(!check){
+                count++;
+            }
+        }
+        return count;
+    }
+    public Student diemGPAcaoNhat(){
+        Student maxGPA = students[0];
+        for(Student st : students){
+            if(maxGPA.getGpa() < st.getGpa()){
+                maxGPA = st;
+            }
+        }
+        return maxGPA;
+    }
+    public Student diemGPAthapNhat(){
+        Student minGPA = students[0];
+        for(Student st : students){
+            if(minGPA.getGpa() > st.getGpa()){
+                minGPA = st;
+            }
+        }
+        return minGPA;
+    }
+}
